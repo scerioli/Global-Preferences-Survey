05185c5e598b92e5a7299052b1ad82f1da9bee3e
Sara Cerioli <cerioli@adference.com>
2020-12-13 21:09:02 +0100

    Added some comments on helper function; adjusted comments on main code; wrote more on WhatWeDid

--- WhatWeDid.md
+++ WhatWeDid.md
@@ -5,26 +5,40 @@
 ### 1.1 Data Collection
 To do
 
+
+1.X Distributions for preferences
+
+
+
 ### 1.2 Create the Models
 
 #### Linear Model on Each Country for Each Preference
-Starting from the complete dataset (complete meaning with removed NA rows), we wanted to reproduce the data plotted in Fig. S2. regarding the gender differences and economic development by preference and by country.
+Starting from the complete dataset (meaning with removed NA rows), we wanted to reproduce the data plotted in Fig. S2. regarding the gender differences and economic development by preference and by country.
 
-In doing this, we realized that a part of the data to realize the article is missing and it can be accessed only after payment of a subscription fee to the Gallup World Poll. We decided therefore to continue the analysis without using 2 of the variables used in the model (education level and household income quintile).
+We realized that a part of the data to reproduce the article is missing and it can be accessed only after payment of a subscription fee to the Gallup World Poll. We decided therefore to continue the analysis without using 2 of the variables used in the model (education level and household income quintile).
 
-We created a linear model using the formula:
+We created a linear model for each country using an expression from the article, omitting the 2 missing variables:
 
-$preference ~ gender + age + age_2 + subj_math_skills$
+preference ~ gender + age + age_2 + subj_math_skills
 
 This resulted in 6 different models (one for each preference measure), having an intercept and 4 weights, each of the weight being related to the variable in the formula above. 
 
-We assume that the article continues using the gender coefficient because this is the coefficient with the major weight. We therefore take this coefficient and plot the logarithm of the average GDP per capita versus this coefficient (one coefficient for each country), for the 6 different preference measurements. When plotting this, we use a linear model to plot the fit, the correlation, and the p-value made on the logarithm of the average GDP per capita versus the gender difference coefficients for each country, to extract relevant statistical information about the variables.
+The weigth for dummy variable "gender" is used as measure of the coutry level gender difference. 
 
-#### Principal Component Analysis
-To summarise the average gender difference among these preferences, we performed -- as the authors -- a principal component analysis on the gender coefficient extracted from the linear model. The PCA is meant to uncorrelate those variables which are correlated, using the rotation of a multi-dimensional plane to extract the vectors where it exists the most variance of each variable. 
+Therefore, in total we have 6 weigths that represent the preference difference related to the gender for 76 countries.
 
-In our case, the variables are the preferences, and we want to extract the information about their principal component to use it as a measure for the average difference in the gender preferences. Therefore, after performing the PCA on our data, we selected the first component and plotted it against the logarithmic average of the GDP per capita for each country.
+We plot the logarithm of the average GDP per capita versus the preference differences, for the 6 different preference measurements. When plotting this, we use a linear model to fit, and extract the correlation and the p-value.
 
-Again, while plotting, we performed a linear regression on the data points, plotting the fit and extracting the correlation and p-value of the average gender difference in preferences versus the logarithm of the average GDP per capita. Moroever, to replicate the results, we adapt the plot to have the minimum and the maximum corresponding to the minimum and maximum found on the y-axis.
+Results
+
+#### Principal Component Analysis#### 
+
+To summarise the average gender difference among these preferences, we performed -- as the authors -- a principal component analysis on the gender preference differences from the linear model and use the first component the summary index of average gender differences in preferences. And then we plotted it against the logarithmic average of the GDP per capita for each country.
+
+We performed a linear regression on the data points, extracting the correlation and p-value of the average gender difference in preferences versus the logarithm of the average GDP per capita. The variables on y-axis were additionaly trasformed as (y-y_min)/(y_max-y_min).
 
 ### 1.3 Discussion On This First Step
+
+We assume that the article continues using the gender coefficient because this is the coefficient with the major weight. 
+The PCA is meant to uncorrelate those variables which are correlated, using the rotation of a multi-dimensional plane to extract the vectors where it exists the most variance of each variable. 
+In our case, the variables are the preferences, and we want to extract the information about their principal component to use it as a measure for the average difference in the gender preferences. Therefore, after performing the PCA on our data
--- functions/helper_functions/ExtractModelSummary.r
+++ functions/helper_functions/ExtractModelSummary.r
@@ -1,41 +1,78 @@
 ExtractModelSummary <- function(dat, var1, var2 = NULL) {
+    # This function has the purpose to extract the important parameters from the
+    # model(s). If var2 is given, it means that there is the need to make the
+    # model run on the same variable grouped by different rows.
+    # 
+    # ARGS
+    # - dat   [data table] is the data we are giving to the model
+    # - var1  [character]  is the column name of the variable we want to put
+    #                      in the model in correlation to log(avgGDPpc)
+    # - var2  [character]  is the column name of the variable that we want to
+    #                      group by. Default is NULL and means that only one
+    #                      model is produced
+    # RETURN
+    # - dt    [data table] a data table with the statistical values of interest.
+    #                      If var2 is not NULL, the number of rows of this data 
+    #                      table is the same number as the unique values in the
+    #                      column given by var2
     
     if (!is.null(var2)) {
         mod <- dlply(dat, var2, function(dt) 
             lm(log(avgGDPpc) ~ eval(as.name(var1)), data = dt))
-        dt <- data.table(formula = character(), correlation = character(), 
-                         r2 = double(), pvalue = character(), 
-                         preference = character())
+        dt <- data.table(formula     = character(), 
+                         correlation = character(), 
+                         r2          = double(), 
+                         pvalue      = character(), 
+                         preference  = character())
+        # For each model, save a data table containing the statistical values
+        # of interest
         for (i in 1:length(mod)) {
+            # Reassign the correct name of the variable
             names(mod[[i]]$coefficients)[2] <- var1
-            formula <- sprintf("italic(y) == %.2f % +.2f * italic(x)",
-                               round(coef(mod[[i]])[1], 5), round(coef(mod[[i]])[2], 5))
-            r <- cor(log(dat[eval(as.name(var2)) == names(mod)[i], avgGDPpc]), 
-                     dat[eval(as.name(var2)) == names(mod)[i], eval(as.name(var1))])
+            formula <- sprintf("y == %.2f % +.2f * x",
+                               round(coef(mod[[i]])[1], 5), 
+                               round(coef(mod[[i]])[2], 5))
+            r <- cor(x = log(dat[eval(as.name(var2)) == names(mod)[i], 
+                                 avgGDPpc]), 
+                     y = dat[eval(as.name(var2)) == names(mod)[i], 
+                             eval(as.name(var1))])
             correlation <- sprintf("correlation = %.5f", r)
             r2 <- sprintf("R^2 = %.5f", r^2)
             p_value <- summary(mod[[i]])$coefficients[,"Pr(>|t|)"][2]
-            pvalue <- ifelse(p_value < 0.0001, "p < 0.0001", sprintf("p = %.4f", p_value))
-            
-            dt_tmp <- data.table(formula = formula, correlation = correlation, r2 = r2, 
-                             pvalue = pvalue, preference = names(mod)[i], 
-                             stringsAsFactors = FALSE)
+            pvalue <- ifelse(p_value < 0.0001, 
+                             "p < 0.0001", 
+                             sprintf("p = %.4f", p_value))
+            # Save the data
+            dt_tmp <- data.table(formula     = formula, 
+                                 correlation = correlation, 
+                                 r2          = r2, 
+                                 pvalue      = pvalue, 
+                                 preference  = names(mod)[i], 
+                                 stringsAsFactors = FALSE)
             dt <- rbind(dt, dt_tmp)
         }
         
     } else {
         mod <- lm(log(avgGDPpc) ~ eval(as.name(var1)), data = dat)
+        # Reassign the correct name of the variable
         names(mod$coefficients)[2] <- var1
-        formula <- sprintf("italic(y) == %.2f % +.2f * italic(x)",
-                           round(coef(mod)[1], 5), round(coef(mod)[2], 5))
-        r <- cor(log(dat$avgGDPpc), dat[, eval(as.name(var1))])
+        formula <- sprintf("y == %.2f % +.2f * x",
+                           round(coef(mod)[1], 5), 
+                           round(coef(mod)[2], 5))
+        r <- cor(x = log(dat$avgGDPpc), 
+                 y = dat[, eval(as.name(var1))])
         correlation <- sprintf("correlation = %.5f", r)
         r2 <- sprintf("R^2 = %.5f", r^2)
         p_value <- summary(mod)$coefficients[,"Pr(>|t|)"][2]
-        pvalue <- ifelse(p_value < 0.0001, "p < 0.0001", sprintf("p = %.4f", p_value))
-        
-        dt <- data.table(formula = formula, correlation = correlation, r2 = r2, 
-                         pvalue = pvalue, stringsAsFactors = FALSE)
+        pvalue <- ifelse(p_value < 0.0001, 
+                         "p < 0.0001", 
+                         sprintf("p = %.4f", p_value))
+        # Save the data
+        dt <- data.table(formula     = formula, 
+                         correlation = correlation, 
+                         r2          = r2, 
+                         pvalue      = pvalue, 
+                         stringsAsFactors = FALSE)
     }
     
     return(dt)
