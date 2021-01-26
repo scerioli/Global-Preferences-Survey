e74420b6a18d6dacbd4b1719749589aab2e9ba25
Sara Cerioli <cerioli@adference.com>
2021-01-26 19:38:56 +0100

    Added files for income

--- functions/LoadData.r
+++ functions/LoadData.r
@@ -1,21 +1,21 @@
 LoadData <- function() {
   
   # Select the directory
-  path_GPS_dir   <- "files/income/GPS_Dataset/GPS_dataset_individual_level/"
-  path_Index_dir <- "files/income/Data_Extract_From_World_Development_Indicators/"
-  path_GEI_dir   <- "files/income/Gender_Equality_Index_Data/"
+  path_dir     <- "files/income/"
+  path_GPS_dir <- paste0(path_dir, "GPS_Dataset/GPS_dataset_individual_level/")
   
   # Load the data
   data          <- read_dta(paste0(path_GPS_dir, "individual_new.dta")) %>%
                     setDT(.)
-  world_area    <- fread(file = paste0(path_GPS_dir, "world_area.txt"), sep = ",")
-  indicators    <- fread(file = paste0(path_Index_dir, "Data.csv"), na = "..")
-  timeWomenSuff <- fread(file = paste0(path_GEI_dir, "Womens_suffrage_date_mod.csv"), 
+  world_area    <- fread(file = paste0(path_GPS_dir, "world_area.txt"), 
+                         sep = ",")
+  indicators    <- fread(file = paste0(path_dir, "Data.csv"), na = "..")
+  timeWomenSuff <- fread(file = paste0(path_dir, "Womens_suffrage_date_mod.csv"), 
                          na.strings = "-") %>% setDT(.)
-  WEF           <- fread(paste0(path_GEI_dir, "WEF_Global_Gender_Gap_Index.txt"),
+  WEF           <- fread(paste0(path_dir, "WEF_Global_Gender_Gap_Index.txt"),
                          sep = "\t")
-  ratioLabor    <- fread(file = paste0(path_GEI_dir, "RatioLaborMF.csv"), skip = 4)
-  UNindex       <- fread(file = paste0(path_GEI_dir, "Gender_Inequality_Index_UN_2015.csv"),
+  ratioLabor    <- fread(file = paste0(path_dir, "RatioLaborMF.csv"), skip = 4)
+  UNindex       <- fread(file = paste0(path_dir, "Gender_Inequality_Index_UN_2015.csv"),
                          na = "..")
   
   return(list(data = data,
