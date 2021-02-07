31aae4435147f6726503d47e12a8c4355de69265
Sara Cerioli <cerioli@adference.com>
2021-02-07 21:00:48 +0100

    Modified the SourceFunctions

--- functions/helper_functions/SourceFunctions.r
+++ functions/helper_functions/SourceFunctions.r
@@ -1,10 +1,8 @@
-SourceFunctions <- function(path, print = TRUE) {
+SourceFunctions <- function(path) {
   # This function sources all the functions in (sub)-directories of path
   # 
   # ARGS
   #   path      [character]  the directory path
-  #   print     [logical]    print sourced files if TRUE (default)
-  #   recursive [logical]    source sub-directories if TRUE (default)
   #   
   # RETURN
   #   it sources all the files into the current environment
@@ -12,14 +10,6 @@ SourceFunctions <- function(path, print = TRUE) {
   files <- list.files(path, pattern = "[.][Rr]$", recursive = TRUE)
   
   for (file in files) {
-    if (print) {
-      cat(file)
-    } 
-    
     source(file.path(path, file), local = FALSE)
-    
-    if (print) {
-      cat("\n")
-    } 
   }
 }
