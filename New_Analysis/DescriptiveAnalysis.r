d50fc67a9e22484e094d4babbe9b1f1fdc81f550
scerioli <sara.cerioli97@gmail.com>
2021-03-07 20:37:21 +0100

    New part of the analysis

--- New_Analysis/DescriptiveAnalysis.r
+++ New_Analysis/DescriptiveAnalysis.r
@@ -15,6 +15,13 @@ SourceFunctions(path = "New_Analysis/functions")
 
 # Load libraries
 LoadRequiredLibraries()
+# library(GGally)
+# library(reshape2)
+# library(lme4)
+# require(compiler)
+# require(parallel)
+# require(boot)
+# require(lattice)
 
 # Load the data
 data_all <- LoadData()
@@ -109,6 +116,51 @@ for(C in unique(dataHeatMap$country)) {
 dataHeatMap[dataComplete, logAvgGDPpc := i.logAvgGDPpc, on = "country"]
 dataHeatMap[dataComplete, mean_trust  := i.mean_trust, 
             on = c("country", "gender")]
+
+
+# Prepare heat map for subjective math skills base
+nSample <-  dataComplete[, .N, by = c("country", "gender")]
+nSample$total <- nSample$N
+nSample$N <- NULL
+
+x <- unique(dataComplete$country)
+y <- min(dataComplete$subj_math_skills):max(dataComplete$subj_math_skills)
+g <- unique(dataComplete$gender)
+dataHeatMap <- expand.grid(country = x, Y = y, gender = g)
+
+dataHeatMap <- merge(dataHeatMap, 
+                     dataComplete[, .N, by = c("subj_math_skills", "country", "gender")],
+                     by.x = c("Y", "country", "gender"), 
+                     by.y = c("subj_math_skills", "country", "gender"))
+# Make it a data table for comfortable use
+setDT(dataHeatMap)
+# Merge with sample
+dataHeatMap[nSample, total := i.total, on = c("country", "gender")]
+dataHeatMap[, fracN := N / total, by = c("country", "gender")]
+# Set the variables to the correct format
+dataHeatMap[, `:=` (Y       = as.factor(Y), 
+                    country = as.character(country), 
+                    fracN   = as.numeric(fracN),
+                    gender  = as.numeric(gender))]
+dataHeatMap[, `:=` (N = NULL, total = NULL)]
+# Fill the missing data
+for(C in unique(dataHeatMap$country)) {
+  for (idx in unique(dataHeatMap$Y)) {
+    for (G in unique(dataHeatMap$gender)) {
+      if (nrow(dataHeatMap[country == C & Y == idx & gender == G]) == 0) {
+        newRow <- data.table(Y = as.numeric(idx), 
+                             country = as.character(C), 
+                             fracN = as.numeric(0),
+                             gender = as.numeric(G))
+        dataHeatMap <- rbind(dataHeatMap, newRow)
+      }
+    }
+  }
+}
+
+dataHeatMap[dataComplete, logAvgGDPpc := i.logAvgGDPpc, on = "country"]
+# dataHeatMap[dataComplete, mean_trust  := i.mean_trust, 
+#             on = c("country", "gender")]
 
 
 # ============= #
@@ -213,6 +265,38 @@ ggplot(difference, aes(x = reorder(country, logAvgGDPpc), y = Y, fill = diffPerc
   xlab("Country (ordered by log GDP p/c)") + ylab("Trust") +
   scale_fill_gradient2(low = "blue", high = "red")
 
+
+
+
+differenceSMS <- dataHeatMap[gender == 1][dataHeatMap[gender == 0], 
+                                       .(diff = fracN - i.fracN,
+                                         diffPerc = (fracN - i.fracN) / fracN,
+                                         Y = i.Y,
+                                         logAvgGDPpc = i.logAvgGDPpc,
+                                         country = i.country),
+                                       on = c("country", "Y")]
+
+
+differenceSMS <- differenceSMS[, maxDiff := max(abs(diff)), by = c("country")]
+
+diff_maxDiff_SMS <- ggplot(differenceSMS) +
+  geom_point(aes(x = reorder(country, maxDiff), y = maxDiff)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  xlab("Country (ordered by absolute difference)") + ylab("")
+
+ggsave(filename = "New_Analysis/plots/diff_maxDiff_subjMathSkills.png",
+         plot = diff_maxDiff_SMS)
+
+diff_heatmap_SMS <- ggplot(differenceSMS) +
+  geom_tile(aes(x = reorder(country, logAvgGDPpc), y = Y, fill = diff)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  scale_fill_gradient2(low = "blue", high = "red") +
+  xlab("Country (ordered by logGDP p/c)") + ylab("")
+ggsave(filename = "New_Analysis/plots/diff_heatmap_subjMathSkills.png",
+       plot = diff_heatmap_SMS)
+
+
+
 ggplot(dataComplete[country == "Guatemala"], aes(x = trustRaw, fill = factor(gender))) +
   geom_histogram(aes(y = 0.5 * ..density..), position = "dodge")
 
@@ -222,4 +306,7 @@ ggplot(prova, aes(x = reorder(country, diffMean),
   geom_point() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   xlab("Country (ordered by mean difference)") + ylab("")
+
+
+ggpairs(dataComplete[, c("gender", "subj_math_skills", "age")])
 
---
+++ New_Analysis/ModelAnalysis.r
@@ -0,0 +1,294 @@
+######################### USE A NEW MODEL ON TRUST # ###########################
+
+# =============================== #
+#### 0. LOAD DATA AND SET PATH ####
+# =============================== #
+
+# Set the path
+setwd("~/Desktop/Projects/Global-Preferences-Survey/")
+
+# Source helper functions
+source("functions/helper_functions/SourceFunctions.r")
+SourceFunctions(path = "functions/")
+SourceFunctions(path = "functions/helper_functions/")
+SourceFunctions(path = "New_Analysis/functions")
+
+# Load libraries
+LoadRequiredLibraries()
+library(MASS)
+require(GGally)
+require(reshape2)
+require(lme4)
+require(compiler)
+require(parallel)
+require(boot)
+require(lattice)
+select <- dplyr::select
+melt <- reshape2::melt
+
+# Load the data
+data_all <- LoadData()
+
+
+# ========================= #
+#### 1. PREPARE THE DATA ####
+# ========================= #
+
+data_all <- PrepareData(data_all)
+
+# Use only the complete dataset
+dataComplete <- data_all$data[complete.cases(data_all$data)]
+
+# Shift the values to have a scale of 0-10
+dataComplete[, trustRaw := round(((trust - min(trust)) / 
+                                    (max(trust) - min(trust))) * 10),
+             by = "country"]
+
+dataComplete[, logAvgGDPpc := log(avgGDPpc), by = "country"]
+dataComplete[, avgGDPpc := NULL]
+
+# Define mean and standard error of the mean for each country and each gender
+dataComplete[, mean_trust := mean(trustRaw, na.rm = T), 
+             by = c("country", "gender")]
+dataComplete[, sem_trust := sd(trustRaw, na.rm = T) / sqrt(.N), 
+             by = c("country", "gender")]
+
+dataComplete[, `:=` (trustRaw = as.factor(trustRaw),
+                     gender = as.factor(gender),
+                     age = as.numeric(age))]
+
+# ======================= #
+#### 2. MODEL ANALYSIS ####
+# ======================= #
+
+# What does influences the results on trust?
+# 
+# Only gender
+model_g <- dlply(dataComplete, "country", function(dt)
+  polr(trustRaw ~ gender, 
+       data = dt, Hess = TRUE))
+# Gender and subjective math skills
+model_gSMS <- dlply(dataComplete, "country", function(dt)
+  polr(trustRaw ~ gender + subj_math_skills, 
+       data = dt, Hess = TRUE))
+# Gender, subjective math skills and age
+model_gSMSa <- dlply(dataComplete, "country", function(dt)
+  polr(trustRaw ~ gender + subj_math_skills + age, 
+       data = dt, Hess = TRUE))
+
+# Linear model (trustRaw must be converted to numeric otherwise it gives problems)
+linear_model <- dlply(dataComplete[, trustRaw := as.numeric(trustRaw)], "country", 
+                      function(dt)
+                        lm(trustRaw ~ gender + subj_math_skills + age, 
+                           data = dt))
+
+# Extract confident intervals from models
+confint_g <- lapply(model_g, function(x) confint(x))
+confint_gSMS <- lapply(model_gSMS, function(x) confint(x))
+confint_gSMSa <- lapply(model_gSMSa, function(x) confint(x))
+confint_linear <- lapply(linear_model, function(x) confint(x))
+
+# Counts how many times GENDER is not including 0 (printing how many times it
+# is relevant)
+# Only gender model
+inf_g <- lapply(confint_g, '[', 1)
+sup_g <- lapply(confint_g, '[', 2)
+containsZero_g <- inf_g <= 0 & sup_g >= 0
+length(containsZero_g[containsZero_g != TRUE])
+# Gender + subjective math skills
+inf_gSMS <- lapply(confint_gSMS, '[', 1)
+sup_gSMS <- lapply(confint_gSMS, '[', 3)
+containsZero_gSMS <- inf_gSMS <= 0 & sup_gSMS >= 0
+length(containsZero_gSMS[containsZero_gSMS != TRUE])
+# Gender + subjective math skills + age
+inf_gSMSa <- lapply(confint_gSMSa, '[', 1)
+sup_gSMSa <- lapply(confint_gSMSa, '[', 4)
+containsZero_gSMSa <- inf_gSMSa <= 0 & sup_gSMSa >= 0
+length(containsZero_gSMSa[containsZero_gSMSa != TRUE])
+# Linear model
+inf_linear <- lapply(confint_linear, '[', 2)
+sup_linear <- lapply(confint_linear, '[', 6)
+containsZero_linear <- inf_linear <= 0 & sup_linear >= 0
+length(containsZero_linear[containsZero_linear != TRUE])
+
+# Counts how many times SUBJ MATH SKILLS is not including 0 (printing how many
+# times it is relevant)
+# Gender + subjective math skills
+inf_gSMS <- lapply(confint_gSMS, '[', 2)
+sup_gSMS <- lapply(confint_gSMS, '[', 4)
+containsZero_gSMS <- inf_gSMS <= 0 & sup_gSMS >= 0
+length(containsZero_gSMS[containsZero_gSMS != TRUE])
+# Gender + subjective math skills + age
+inf_gSMSa <- lapply(confint_gSMSa, '[', 2)
+sup_gSMSa <- lapply(confint_gSMSa, '[', 4)
+containsZero_gSMSa <- inf_gSMSa <= 0 & sup_gSMSa >= 0
+length(containsZero_gSMSa[containsZero_gSMSa != TRUE])
+# Linear model
+inf_linear <- lapply(confint_linear, '[', 3)
+sup_linear <- lapply(confint_linear, '[', 7)
+containsZero_linear <- inf_linear <= 0 & sup_linear >= 0
+length(containsZero_linear[containsZero_linear != TRUE])
+
+# Counts how many times AGE is not including 0 (printing how many times it is 
+# relevant)
+# Gender + subjective math skills + age
+inf_gSMSa <- lapply(confint_gSMSa, '[', 3)
+sup_gSMSa <- lapply(confint_gSMSa, '[', 6)
+containsZero_gSMSa <- inf_gSMSa <= 0 & sup_gSMSa >= 0
+length(containsZero_gSMSa[containsZero_gSMSa != TRUE])
+# Linear model
+inf_linear <- lapply(confint_linear, '[', 4)
+sup_linear <- lapply(confint_linear, '[', 8)
+containsZero_linear <- inf_linear <= 0 & sup_linear >= 0
+length(containsZero_linear[containsZero_linear != TRUE])
+
+# In general, the subjective math skill is relevant for almost all the countries
+# (except for one/two) for what concerns the trust, while the gender influences
+# even less than the age (25 non-zero vs. 35 non-zero).
+
+
+# Comparing the model shows that using the gender + SMS is better, sometimes
+# also age is improving the information
+AIC_models <- Map(function(gender, genderSMS, genderSMSage, linear)
+  AIC(gender, genderSMS, genderSMSage, linear), 
+  model_g, model_gSMS, model_gSMSa, linear_model)
+
+
+# What about plotting the gender coefficient for the new model as they did, 
+# and see the difference to what they have with the linear model?
+
+dt_coefficients <- c()
+
+# Extract gender coefficient from linear model
+for (i in 1:length(linear_model)) {
+  dt_tmp <- data.table(country = names(linear_model)[[i]],
+                       genderCoeff = coef(linear_model[[i]])[[2]],
+                       SMSCoeff = coef(linear_model[[i]])[[3]],
+                       ageCoeff = coef(linear_model[[i]])[[4]],
+                       inf_g = inf_linear[[i]],
+                       sup_g = sup_linear[[i]],
+                       modelType = "linear")
+  dt_coefficients <- rbind(dt_coefficients, dt_tmp)
+}
+
+# Extract gender coefficient from ordered logit model
+for (i in 1:length(model_gSMSa)) {
+  dt_tmp <- data.table(country = names(model_gSMSa)[[i]],
+                       genderCoeff = coef(model_gSMSa[[i]])[[1]],
+                       SMSCoeff = coef(model_gSMSa[[i]])[[2]],
+                       ageCoeff = coef(model_gSMSa[[i]])[[3]],
+                       inf_g = inf_gSMSa[[i]],
+                       sup_g = sup_gSMSa[[i]],
+                       modelType = "ordlog")
+  dt_coefficients <- rbind(dt_coefficients, dt_tmp)
+}
+
+dataToPlot <- dt_coefficients[dataComplete, `:=` (logAvgGDPpc = i.logAvgGDPpc,
+                                                  isocode = i.isocode), 
+                              on = "country"]
+
+ggplot(dataToPlot, aes(x = logAvgGDPpc, y = genderCoeff)) +
+  geom_point(aes(x = logAvgGDPpc, y = genderCoeff)) +
+  geom_errorbar(aes(ymin = genderCoeff - abs(inf_g), 
+                    ymax = genderCoeff + abs(sup_g)), width = .05) +
+  xlab("log GDP p/c") + ylab("Gender Coefficient") +
+  facet_grid(~ modelType)
+
+ggplot(dataToPlot) +
+  geom_point(aes(x = logAvgGDPpc, y = SMSCoeff)) +
+  facet_wrap(~ modelType)
+
+PlotSummary(data = dataToPlot, var1 = "logAvgGDPpc", var2 = "genderCoeff",
+            var3 = "modelType",
+            regression = TRUE, display = T)
+
+# What about the average age within the dataset?
+dataComplete[, meanAge := as.numeric(mean(age)), by = c("country", "gender")]
+
+ggplot(dataComplete[, .(logAvgGDPpc, meanAge, gender, country)]) +
+  geom_point(aes(x = logAvgGDPpc, y = meanAge)) +
+  geom_smooth(aes(x = logAvgGDPpc, y = meanAge), method = "lm") +
+  facet_grid(~ gender)
+
+
+PlotSummary(data = dataComplete, var1 = "logAvgGDPpc", var2 = "meanAge",
+            var3 = "gender",
+            regression = TRUE, display = F, 
+            save = "New_Analysis/plots/trust_logGDP_meanAge.png")
+
+# The age is higher for the more developed countries, and the age of female is 
+# higher with respect to the male age for more developed countries.
+# Can this explain the differences?
+# What about subjective math skills?
+
+dataComplete[, meanSMS := mean(subj_math_skills), by = c("country", "gender")]
+
+ggplot(dataComplete[, .(logAvgGDPpc, meanSMS, gender, country)]) +
+  geom_point(aes(x = logAvgGDPpc, y = meanSMS)) +
+  geom_smooth(aes(x = logAvgGDPpc, y = meanSMS), method = "lm") +
+  facet_grid(~ gender)
+
+
+# Let's try another model: Mixed Effects Logistic Regression
+# Model with gender and subjective math skills as level
+# categorical predictor, and age with a random intercept
+glme_gSMSa <- glmer(trustRaw ~ gender + subj_math_skills + age + (1 | country) + (1 | region),
+                    data = dataComplete, family = binomial,
+                    control = glmerControl(optimizer = "bobyqa"),
+                    nAGQ = 1)
+
+
+lattice::dotplot(ranef(glme_gSMSa, which = "country", condVar = TRUE))
+lattice::dotplot(ranef(glme_gSMSa, which = "region", condVar = TRUE))
+
+se <- sqrt(diag(vcov(glme_gSMSa)))
+# table of estimates with 95% CI
+tab <- cbind(Est = fixef(glme_gSMSa), LL = fixef(glme_gSMSa) - 1.96 * se, 
+             UL = fixef(glme_gSMSa) + 1.96 *se)
+
+
+# ============================= #
+#### 3. MODEL INTERPRETATION ####
+# ============================= #
+
+# Interpreting the model & visualize the changes
+newdat <- data.table(gender = factor(rep(0:1, 110)),
+                     subj_math_skills = rep(0:10, each = 100))
+
+new <- c()
+
+for (i in 1:length(model_gSMSa)) {
+  tmp <- cbind(newdat, predict(model_gSMS[[i]], newdat, type = "probs"),
+               country = names(model_gSMS)[[i]])
+  
+  new <- rbind(new, tmp)
+}
+
+
+lnewdat <- melt(new, id.vars = c("gender", "subj_math_skills", "country"),
+                variable.name = "Trust", value.name = "Probability")
+
+
+setDT(lnewdat)
+lnewdat[, Probability := strtrim(Probability, 8)]
+lnewdat[, Probability := as.numeric(Probability)]
+lnewdat[, gender := int(gender)]
+
+set.seed(123)
+randomSample <- lnewdat[sample(1:nrow(lnewdat), 8000), ]
+setDT(randomSample)
+randomSample[, Probability := strtrim(Probability, 8)]
+randomSample[, Probability := as.numeric(Probability)]
+randomSample[, gender := int(gender)]
+
+ggplot(lnewdat[country %in% c("Russia")], 
+       aes(x = gender, y = Probability, col = Trust)) +
+  geom_line() +
+  facet_wrap(~ subj_math_skills)
+
+prova <-randomSample
+setkey(prova, gender, country)
+prova[, diff := Probability - shift(Probability, fill = max(Probability)), by = gender]
+
+
+
