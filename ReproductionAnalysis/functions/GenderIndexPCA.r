ada7db9b0e3a02b273c2768e77cffbf8d668a9c2
scerioli <sara.cerioli97@gmail.com>
2022-01-15 19:17:28 +0100

    This branch is the cleaned version of the extended analysis and can be merged safely to master

--- ReproductionAnalysis/functions/GenderIndexPCA.r
+++
@@ -1,9 +0,0 @@
-GenderIndexPCA <- function(data) {
-  
-  pcaColumns <- imputePCA(data, method = "EM", ncp = 1)
-  pca <- prcomp(pcaColumns$completeObs, scale. = T)
-  
-  genderIndex <- pca$x[, 1]
-  
-  return(genderIndex)
-}
--- ReproductionAnalysis/functions/helper_functions/CompareCorrelationCoefficients.r
+++
@@ -1,9 +0,0 @@
-CompareCorrelationCoefficients <- function(corr1, corr2, n1, n2) {
-  
-  r_prime1 <- 0.5 * log((1 + corr1) / (1 - corr1))
-  r_prime2 <- 0.5 * log((1 + corr2) / (1 - corr2))
-  
-  z <- (r_prime1 - r_prime2) / sqrt((1 / (n1 - 3)) + (1 / (n2 - 3)))
-  
-  return(z)
-}
